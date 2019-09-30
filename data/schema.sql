ee2796ea767b724c40035c083a4398cc5468f597
Jonathan A. Kimball <jonathan.kimball64@gmail.com>
2019-09-30 16:54:11 -0700

    inital running server

--- data/schema.sql
+++ data/schema.sql
@@ -5,11 +5,10 @@ CREATE DATABASE jblm_unlimited_cf19;
 
 DROP TABLE IF EXISTS all_Events;
 
-CREATE TABLE all_Events{
+CREATE TABLE all_Events (
     id SERIAL PRIMARY KEY,
     event_title varchar(255),
     event_date varchar(255),
     event_description varchar(255),
-    event_link varchar 255)
-
-};
+    event_link varchar (255)
+);
