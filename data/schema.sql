ed38a6a70e57087d0b64d096de9f4da1753e975b
Jonathan A. Kimball <jonathan.kimball64@gmail.com>
2019-10-01 12:43:35 -0700

    schema for resourses including logo images and resourse files (PDF)

--- data/schema.sql
+++ data/schema.sql
@@ -3,12 +3,29 @@ CREATE DATABASE jblm_unlimited_cf19;
 \c jblm_unlimited_cf19;
 
 
-DROP TABLE IF EXISTS all_Events;
+DROP TABLE IF EXISTS event;
 
-CREATE TABLE all_Events (
-    id SERIAL PRIMARY KEY,
-    event_title varchar(255),
-    event_date varchar(255),
-    event_description varchar(255),
-    event_link varchar (255)
+CREATE TABLE event (
+  id SERIAL PRIMARY KEY,
+  event_title varchar(255),
+  event_date varchar(255),
+  event_description varchar(255),
+  event_link varchar (255)
 );
+
+CREATE TABLE resource (
+  id SERIAL PRIMARY KEY,
+  title varchar(255),
+  description text,
+  resource_url varchar(255),
+  logo_png bytea
+);
+
+CREATE TABLE resource_file (
+  id SERIAL PRIMARY KEY,
+  resource_id int,
+  file_name varchar(255),
+  file_content bytea
+);
+
+ALTER TABLE resource_file ADD CONSTRAINT fk_resource FOREIGN KEY (resource_id) REFERENCES resource(id);
